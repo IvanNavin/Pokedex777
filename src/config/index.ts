e64041d5c90860637a32ee68d9f3fd6d1b0a3fc1
Toricheli <t0637242529@gmail.com>
2021-03-06 13:41:45 +0200

    changed http to https

--- src/config/index.ts
+++ src/config/index.ts
@@ -39,7 +39,7 @@ interface IConfig {
 const config: IConfig = {
   client: {
     server: {
-      protocol: 'http',
+      protocol: 'https',
       host: 'zar.hosthot.ru',
     },
     endpoint: {
